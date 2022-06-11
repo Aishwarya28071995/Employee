80aa2064d5134d35c7e6262a3bfac8a30a8ce27c
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 21:15:55 +0530

    Commiting files for UC4

--- EmployeeWageProgram/EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/EmployeeWageProgram/Program.cs
@@ -10,25 +10,25 @@ namespace EmployeeWageProgram
             Program program = new Program();
             program.CalculateFullTimeWage();
 
-        }   
+        }
 
-            public void CalculateFullTimeWage()
+        public void CalculateFullTimeWage()
+        {
+            int WagePerHr = 20;
+            Random random = new Random();
+            int value = random.Next(0, 2);
+            if (value == 0)
             {
-                int WagePerHr = 20;
-                Random random = new Random();
-                int value = random.Next(0, 2);
-                if (value == 0)
-                {
-                    Console.WriteLine("Employee is absent");
-                }
-                else
-                {
-                    int Salary = WagePerHr * 8;
-                    Console.WriteLine(Salary);
-                }
-
-
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                int Salary = WagePerHr * 8;
+                Console.WriteLine(Salary);
             }
 
+
         }
+
+    }
 }
